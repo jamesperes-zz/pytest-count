9fde128852cdb28dfbbec99e2f267203be4d9dcb
jamesperes <james.peres.s@gmail.com>
2018-01-03 14:32:34 +0000

    up

--- README.md
+++
@@ -1,2 +0,0 @@
-# pytest-count
-Plugin for Pytest - count number test fails and warning when number fails tests up
--- pytest_count.py
+++ pytest_count.py
@@ -1,21 +1,38 @@
 # -*- coding: utf-8 -*-
 
 import pytest
+import os.path
+from datetime import date
+
+today = str(date.today())
+
+with open("failures", "a") as d:
+    d.write("\n\n" + "=" * 10 + "Teste rodando " + today + "=" * 10 + "\n\n")
 
 
 def pytest_addoption(parser):
     group = parser.getgroup('count')
     group.addoption(
-        '--foo',
+        '--count',
         action='store',
-        dest='dest_foo',
-        default='2018',
-        help='Set the value for the fixture "bar".'
+        dest='counter',
+        default='False',
+        help='Show number of erros '
     )
 
     parser.addini('HELLO', 'Dummy pytest.ini setting')
 
 
-@pytest.fixture
-def bar(request):
-    return request.config.option.dest_foo
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    if rep.when == "call" and rep.failed:
+        mode = "a" if os.path.exists("failures") else "w"
+        with open("failures", mode) as f:
+            if "tmpdir" in item.fixturenames:
+                extra = " (%s)" % item.funcargs["tmpdir"]
+            else:
+                extra = ""
+            f.write(rep.nodeid + extra + ' falhas ' + today + "\n")
