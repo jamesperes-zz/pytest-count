23ba88515d5895a0d2067c1e81cc56bae302da81
James Peres <james.peres.s@gmail.com>
2018-01-08 11:41:43 +0000

    add json

--- pytest_count.py
+++ pytest_count.py
@@ -2,12 +2,17 @@
 
 import pytest
 import os.path
+import json
+
 from datetime import date
+
 
 today = str(date.today())
 
-with open("failures", "a") as d:
-    d.write("\n\n" + "=" * 10 + "Teste rodando " + today + "=" * 10 + "\n\n")
+with open("failures.json") as json_data:
+    d = json.loads(json_data)
+    for p in d['erros']:
+        print(p['id'])
 
 
 def pytest_addoption(parser):
@@ -30,10 +35,14 @@ def pytest_runtest_makereport(item, call):
     rep = outcome.get_result()
     if rep.when == "call" and rep.failed:
         count += 1
-        mode = "a" if os.path.exists("failures") else "w"
-        with open("failures", mode) as f:
+        data = {}
+        data['erros'] = []
+        mode = "a" if os.path.exists("failures.json") else "w"
+        with open("failures.json", mode) as f:
             if "tmpdir" in item.fixturenames:
                 extra = " (%s)" % item.funcargs["tmpdir"]
             else:
                 extra = ""
-            f.write(rep.nodeid + extra + ' falhas ' + today + "\n")
+            data['erros'].append({'id': rep.nodeid,
+                                  'extra': extra})
+            json.dump(data, f)
